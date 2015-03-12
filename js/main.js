f85e1a31d9fa49d81c9317a2694724b677f8f1a1
Renaud Fontana <sirgzu@hotmail.com>
2015-03-12 09:31:47 +0000

    Custom elements now loading successfully
    Moved from XHR to link to import `polymer.html`. It's now deduping `polymer.js` as it should.

--- js/main.js
+++ js/main.js
@@ -15,41 +15,11 @@ require.config({
 	}
 });
 
-require(['libs/d3/d3', 'libs/q/q'], function(d3, Q){
-	var polymerReady = Q.defer();
-
-	var request = d3.html('libs/polymer/polymer.html', function(d){
-
-		var links = d.querySelectorAll('link');
-		var linksDeferred = [];
-		Array.prototype.forEach.call(links, function(link, i) {
-			var linkDeferred = Q.defer();
-			var urlTokens = link.href.split('/');
-			link.href = 'libs/polymer/'+urlTokens[urlTokens.length-1];
-			link.onload = function(e) { linkDeferred.resolve(); }
-			linksDeferred.push(linkDeferred.promise);
-		});
-
-		var scripts = d.querySelectorAll('script');
-		var scriptsDeferred = [];
-		Array.prototype.forEach.call(scripts, function(script, i) {
-			var scriptDeferred = Q.defer();
-			var urlTokens = script.src.split('/');
-			script.src = 'libs/polymer/'+urlTokens[urlTokens.length-1];
-			script.onload = function(e) { scriptDeferred.resolve(); }
-			scriptsDeferred.push(scriptDeferred.promise);
-		});
-
-		Q.all(linksDeferred.concat(scriptsDeferred)).done(function(){
-			console.log('Polymer is fully loaded');
-			polymerReady.resolve();
-		});
-		document.head.appendChild(d);
-	});
-
-	Q.when(polymerReady.promise).done(function(){
-		Polymer.import(['elements/my-app.html'], function(){
-			debugger;
-		});
-	});
+require([], function(){
+	var link = document.createElement('link');
+	link.rel = 'import';
+	link.href = 'libs/polymer/polymer.html';
+	link.onload = function(e){ Polymer.import(['elements/my-app.html'], function(){}); }
+	var head = document.querySelector('head');
+	head.appendChild(link);
 });
